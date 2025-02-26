ef304150b5a815e26eb0b4d2725fbe2dcfa2daab
Lucas Peixoto <lspeixotodev@gmail.com>
2025-02-26 08:05:53 -0300

    base project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {
+  ActivatedRoute,
+  provideRouter,
+  RouterLink,
+  RouterOutlet,
+  withViewTransitions,
+} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { routes } from './app.routes';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, ReactiveFormsModule],
+      providers: [provideRouter(routes, withViewTransitions())],
     }).compileComponents();
   });
 
